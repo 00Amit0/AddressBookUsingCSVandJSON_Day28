fe0cd9c9abc4502a4133e824dea9e9446be255a2
“00Amit0” <amit836961@gmail.com>
2022-07-06 00:26:30 +0530

    Adding and Deleting details of person

--- AddressBook.cs
+++ AddressBook.cs
@@ -79,6 +79,8 @@ namespace AddressBookUsingCSVandJSON
             listPersonInCity.ForEach(x => Console.WriteLine("{0}\t", x.Name.ToString()));
 
         }
+
+        //UC5 and 6 are done already as the multiple contacts are added in the addressbook
     }
 }
   
